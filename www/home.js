a1b85c2cf8f815c8e17dda18863ed5c6c1d96121
Raghavendra <raghava.u@gmail.com>
2016-06-21 12:20:31 +0530

    new files and folders added

---
+++ www/home.js
@@ -0,0 +1,33 @@
+﻿(function (exports) {
+
+    exports.loadTabHome = function ($scope) {
+	if ($scope.ready) {
+	    $scope.stopPicker();
+	} else {
+	    document.addEventListener('deviceready', $scope.stopPicker);
+	}
+
+        $scope.setMargin("0%", "0%", "0%", "50%");
+        $scope.setLandscapeMargin("0%", "0%", "0%", "50%");
+        var scanResults = document.getElementById('scanResults');
+        $scope.setCallback(function (session, manual) {
+           $scope.pausePicker();
+           if (manual) {
+               var code = session;
+               $scope.$apply(function () {
+                   $scope.scannedCode = 'Manual entry: ' + code;
+               });
+           } else {
+               var code = session.newlyRecognizedCodes[0];
+               $scope.$apply(function () {
+                   $scope.scannedCode = '(' + code.symbology.toUpperCase() + ') ' + code.data;
+                });
+            }
+        });
+        if ($scope.ready) {
+           $scope.startPicker();
+       } else {
+            document.addEventListener('deviceready', $scope.startPicker);
+       }
+    }
+})(this);
